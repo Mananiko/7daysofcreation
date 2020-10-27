644370483bda5e211d32e20901aae267f3a4774e
2manydisplays <der-hoffmann@gmx.de>
2020-10-27 20:47:32 +0100

    Created Shortcut "G" to Toggle GameObjects in Editor

---
+++ 7days/Assets/Editor/EditorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorHelper : MonoBehaviour
+{
+    [MenuItem("GameObject/Toggle Selected GameObject _g")]
+    static void ToggleGameObject()
+    {
+        //var selected = Selection.transforms;
+        if (Selection.transforms != null)
+        {
+            foreach (Transform t in Selection.transforms)
+                t.gameObject.SetActive(!t.gameObject.activeInHierarchy);
+        }
+    }
+
+}
