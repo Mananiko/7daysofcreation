2eff75fdbf8a8abd85c3e4a070ace4b21ddda157
2manydisplays <der-hoffmann@gmx.de>
2020-11-01 17:30:23 +0100

    Added First Person Controller. ToDo: Fix Telport Bug (falling through floor when teleport)

--- 7days/Assets/WebXR/Samples/Desert/Scripts/DesertFreeFlightController.cs
+++ 7days/Assets/WebXR/Samples/Desert/Scripts/DesertFreeFlightController.cs
@@ -2,6 +2,14 @@
 using WebXR;
 
 public class DesertFreeFlightController : MonoBehaviour {
+
+    [SerializeField]
+    public Transform character;
+    Vector2 currentMouseLook;
+    Vector2 appliedMouseDelta;
+    public float sensitivity = 1;
+    public float smoothing = 2;
+
     [Tooltip("Enable/disable rotation control. For use in Unity editor only.")]
     public bool rotationEnabled = true;
 
@@ -63,17 +71,15 @@ public class DesertFreeFlightController : MonoBehaviour {
 
         if (rotationEnabled && Input.GetMouseButton(0))
         {
+            // Get smooth mouse look.
+            Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
+            appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
+            currentMouseLook += appliedMouseDelta;
+            currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
 
-            rotationX += Input.GetAxis ("Mouse X") * mouseSensitivity;
-            rotationY += Input.GetAxis ("Mouse Y") * mouseSensitivity;
-
-            rotationX = ClampAngle (rotationX, minimumX, maximumX);
-            rotationY = ClampAngle (rotationY, minimumY, maximumY);
-
-            Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
-            Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, Vector3.left);
-
-            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+            // Rotate camera and controller.
+            transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
         }
     }
 
