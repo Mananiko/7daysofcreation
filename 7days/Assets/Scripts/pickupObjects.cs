7293caa3f0674b7f494892f0018f3e5b7756e182
2manydisplays <der-hoffmann@gmx.de>
2020-11-13 15:21:34 +0100

    Added attach script for pickupable Objects

--- 7days/Assets/Scripts/pickupObjects.cs
+++ 7days/Assets/Scripts/pickupObjects.cs
@@ -6,6 +6,7 @@ public class pickupObjects : MonoBehaviour
 {
     bool pickedUp = false;
     GameObject pickedUpObject;
+    attatchMe attachScript;
 
     private void Update()
     {
@@ -17,27 +18,31 @@ public class pickupObjects : MonoBehaviour
         if (other.gameObject.CompareTag("Interactable") && pickedUp == false && Input.GetMouseButtonDown(0))
         {
             pickUp(other.gameObject);
+            //other.gameObject.GetComponent<attatchMe>().enabled = true;
             pickedUpObject = other.gameObject;
+
         }                    
     }
-
-    void pickUp(GameObject gameObject)
+    //pick up pickubable object
+    public void pickUp(GameObject gameObject)
     {
-        gameObject.transform.SetParent(Camera.main.transform); //sets the parent of the object to camera (object will be always in front of it)
+        gameObject.transform.SetParent(this.transform); //sets the parent of the object to camera (object will be always in front of it)
         gameObject.gameObject.GetComponent<Rigidbody>().isKinematic = true;                                                      //turns Kinematic on, so object will not interfere when picked up
         gameObject.transform.localPosition = new Vector3(4f, -1.5f, 5f);                      //sets local position in front of player, so it seems player is holding it
         gameObject.transform.localRotation = Quaternion.Euler(0, 0f, 0);                       //rotates the object
-        gameObject.gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+        //check if other GameObject has attachMe Script and deactivate if yes
+        gameObject.gameObject.GetComponent<attatchMe>().attached = true;              
         pickedUp = true;
     }
-
+    //put down pickubable object
     void putDown(GameObject gameObject)
     {
-        if (Input.GetMouseButtonDown(1) && pickedUp == true)                            //if player clicked 'E' and picked up object before
+        if (Input.GetMouseButtonDown(1) && pickedUp == true)                            //if player clicked 'Right Mousebutton' and picked up object before
         {
             gameObject.transform.SetParent(null);                                                  //unleashes object from its parent
             gameObject.GetComponent<Rigidbody>().isKinematic = false;          //turns kinematic off, so object can lightly fall on the ground
-            gameObject.gameObject.GetComponent<BoxCollider>().enabled = true;  
+            gameObject.gameObject.GetComponent<BoxCollider>().isTrigger = false;  
             pickedUp = false;                                                           //object is not picked up
         }
     }
