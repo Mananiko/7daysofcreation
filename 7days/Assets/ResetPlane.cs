40ebb36d98fbf90677717e9a1faa4b341aee0342
2manydisplays <der-hoffmann@gmx.de>
2020-11-29 20:53:42 +0100

    Added PlayerReset Planes to prevent falling down into the void

---
+++ 7days/Assets/ResetPlane.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlane : MonoBehaviour
+{
+    GameObject player;
+    Vector3 starpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        starpos = player.transform.position;
+    }   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+         player.gameObject.transform.position = starpos;
+        }
+       
+    }
+}
---
+++ 7days/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 7)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 7);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
