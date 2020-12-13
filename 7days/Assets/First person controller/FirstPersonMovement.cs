b673e7d1026f2b0285cf42fbe6e65c32b5424594
2manydisplays <der-hoffmann@gmx.de>
2020-12-13 18:30:57 +0100

    PlayerController: Added ESC Key to Quit

--- 7days/Assets/First person controller/FirstPersonMovement.cs
+++ 7days/Assets/First person controller/FirstPersonMovement.cs
@@ -11,5 +11,10 @@ public class FirstPersonMovement : MonoBehaviour
         velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(velocity.x, 0, velocity.y);
-    }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }       
 }
