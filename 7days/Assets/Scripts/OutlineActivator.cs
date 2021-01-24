c78d787593cd88b2d72c42b53e3d0f02c15ab9b9
2manydisplays <der-hoffmann@gmx.de>
2021-01-24 23:59:43 +0100

    Day 7 - Added Precomputed Outlines to Interactables, Added Outline Activator to Player

---
+++ 7days/Assets/Scripts/OutlineActivator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineActivator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (Transform child in other.transform.GetComponentsInChildren<Transform>())
+        {
+            if (child.gameObject.GetComponent<Outline>() != null)
+            {
+                child.gameObject.GetComponent<Outline>().enabled = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        foreach (Transform child in other.transform.GetComponentsInChildren<Transform>())
+        {
+            if (child.gameObject.GetComponent<Outline>() != null)
+            {
+                child.gameObject.GetComponent<Outline>().enabled = false;
+            }
+        }
+    }
+}
